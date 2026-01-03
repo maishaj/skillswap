489004fe89a9c146ef56a8e150bbd0f93abf403b
maishaj <maishajannat388@gmail.com>
2026-01-03 12:55:58 +0600

    added animation and swiper

--- src/components/ToyNavbar.jsx
+++ src/components/ToyNavbar.jsx
@@ -1,60 +1,93 @@
-import React, { use } from 'react';
-import { Link } from 'react-router';
-import { AuthContext } from '../provider/AuthProvider';
+import React, { use } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
+import "animate.css";
 
 const ToyNavbar = () => {
+  const { user, logout } = use(AuthContext);
 
-    const {user,logout}=use(AuthContext);
+  const handleLogout = () => {
+    logout()
+      .then(() => {})
+      .catch((error) => {
+        alert(error);
+      });
+  };
+  console.log(user);
 
-        const handleLogout = () => {
-                logout().then(() => {
-
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-        }
-        console.log(user);
-
-
-    return (
-           <div className=''>
-            <div className="navbar bg-base-100 shadow-sm">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/profile">My Profile</Link></li>
-                    </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">SkillSwap</a>
-                </div>
-
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">My Profile</Link></li>
-                    </ul>
-                </div>
-
-                <div className="navbar-end space-x-2">
-                    <img className='w-10 h-10 rounded-full' src={`${user?user.photoURL:"https://img.icons8.com/?size=100&id=7819&format=png"}`} alt="" />
-                    {
-                        user?(
-                          <button onClick={handleLogout} className='btn'>Logout</button>
-                        ):(
-                          <Link to="/auth/login" className='btn'>Login</Link>
-                        )
-                    }
-                </div>
+  return (
+    <div className="">
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
             </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">My Profile</Link>
+              </li>
+            </ul>
+          </div>
+          <a className="animate__animated animate__bounce btn btn-ghost text-xl">
+            SkillSwap
+          </a>
         </div>
-    );
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">My Profile</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-end space-x-2">
+          <img
+            className="w-10 h-10 rounded-full"
+            src={`${
+              user
+                ? user.photoURL
+                : "https://img.icons8.com/?size=100&id=7819&format=png"
+            }`}
+            alt=""
+          />
+          {user ? (
+            <button onClick={handleLogout} className="btn">
+              Logout
+            </button>
+          ) : (
+            <Link to="/auth/login" className="btn">
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ToyNavbar;
